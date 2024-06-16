db797d48ff8176f990f91602eac0ebd73eeadc85
이충현 <a8207637@naver.com>
2024-06-17 01:35:54 +0900

    feat : add case1 controller, service 카테고리별 최저가 조회 추가

--- product-app/src/main/java/com/musinsa/productapp/product/controller/ProductController.java
+++ product-app/src/main/java/com/musinsa/productapp/product/controller/ProductController.java
@@ -1,4 +1,22 @@
 package com.musinsa.productapp.product.controller;
 
+import com.musinsa.productapp.product.model.entity.Product;
+import com.musinsa.productapp.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/lowest-price")
+    public List<Product> getCategoriesWithLowestPriceProducts() {
+        return productService.getCategoriesWithLowestPriceProducts();
+    }
 }
--- product-app/src/main/java/com/musinsa/productapp/product/service/ProductService.java
+++ product-app/src/main/java/com/musinsa/productapp/product/service/ProductService.java
@@ -1,4 +1,25 @@
 package com.musinsa.productapp.product.service;
 
+import com.musinsa.productapp.product.model.entity.Product;
+import com.musinsa.productapp.product.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getCategoriesWithLowestPriceProducts() {
+        List<Product> allProducts = productRepository.findAll();
+        return allProducts.stream()
+                .collect(Collectors.groupingBy(product -> product.getCategory().getName()))
+                .values().stream()
+                .map(products -> products.stream().min((p1, p2) -> Double.compare(p1.getPrice(), p2.getPrice())).orElse(null))
+                .sorted((p1, p2) -> Double.compare(p1.getPrice(), p2.getPrice()))
+                .collect(Collectors.toList());
+    }
 }
